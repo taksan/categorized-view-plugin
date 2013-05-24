7b8f1f86f6483e02538fcc41de60e4db19c5da0f
takeuchi <g.takeuchi@gmail.com>
2013-05-23 23:03:31 -0300

    Changed way of storing collapse state in the cookie to use json. Fixed bug that was preventing the items to show as collapsed in the first presentation

--- src/main/webapp/sectionToggle.js
+++ src/main/webapp/sectionToggle.js
@@ -1,7 +1,6 @@
 function restoreJobGroupCollapseState(viewName, groupName) 
 {
 	var collapseState = getGroupState(viewName,groupName);
-	if (collapseState == null) collapseState = 'none';
 	
 	if (collapseState == 'none') {
 		hideJobGroup(viewName,groupName)
@@ -13,7 +12,7 @@ function restoreJobGroupCollapseState(viewName, groupName)
 
 function toggleJogGroupVisibility(handle, viewName, group) 
 {
-	if (handle.innerHTML == '[+]') {
+	if (handle.getAttribute("collapseState") == "collapsed") {
 		showJobGroup(viewName,group)
 	}
 	else {
@@ -22,54 +21,47 @@ function toggleJogGroupVisibility(handle, viewName, group)
 }
 
 function hideJobGroup(viewName, group) {
-	$$("#handle_"+group).first().innerHTML = '[+]';
+	$$("#handle_"+group).first().setAttribute("collapseState", "collapsed");
 	$$('.'+group).each(
 		function(e){
 			e.parentNode.style.display="none"
 		}
 	)
 	setGroupState(viewName,group, "none");
+	$$("#handle_"+group).first().innerHTML = '[+]';
 }
 
 function showJobGroup(viewName, group) {
-	$$("#handle_"+group).first().innerHTML = '[-]';
+	$$("#handle_"+group).first().setAttribute("collapseState", "expanded");
 	$$('.'+group).each(
 		function(e){
 			e.parentNode.style.display=""
 		}
 	)
 	setGroupState(viewName, group, "");
+	$$("#handle_"+group).first().innerHTML = '[-]';	
 }
 
 function getGroupStates(viewName) {
 	var stateCookie = YAHOO.util.Cookie.get("jenkins.categorized-view-collapse-state_"+viewName);
 	if (stateCookie == null)
-		return null;
-	return stateCookie.split(" ");
+		return {};
+	return JSON.parse(stateCookie);
 }
 
 function getGroupState(viewName, groupName) {
 	var groupStates = getGroupStates(viewName)
-	if (groupStates == null)
+	
+	if (groupStates[groupName]==null) {
+		setGroupState(viewName, groupName, "none");
 		return "none";
-	if (groupStates.indexOf(groupName)!=-1)
-		return "none";
-	return "";
+	}
+	return groupStates[groupName];
 }
 
 function setGroupState(viewName, groupName, state) 
 {
 	var groupStates = getGroupStates(viewName)
-	if (groupStates == null)
-		groupStates = [];
-		
-	if (state == "none" && (groupStates.indexOf(groupName)==-1)) {
-		groupStates[groupStates.length]=groupName;
-		YAHOO.util.Cookie.set("jenkins.categorized-view-collapse-state_"+viewName, groupStates.join(" "));
-	} 
-	if (state == "" && (groupStates.indexOf(groupName)!=-1)) {
-		var index = groupStates.indexOf(groupName);
-		groupName.splice(index, 1);
-		YAHOO.util.Cookie.set("jenkins.categorized-view-collapse-state_"+viewName, groupStates.join(" "));
-	}
+	groupStates[groupName]=state
+	YAHOO.util.Cookie.set("jenkins.categorized-view-collapse-state_"+viewName, Object.toJSON(groupStates));
 }
